4795979964edda69eb1888767866499d3fa03f7b
Zach Cervi <zachcervi@gmail.com>
2023-09-06 15:04:12 -0600

    Error Handling

---
+++ Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Zach Cervi on 9/6/23.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+    
+    init(id: UUID = UUID(), error: Error, guidance: String){
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
